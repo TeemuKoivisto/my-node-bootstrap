b4432fda12d554b1fbc824ce446053923c826a4c
Teemu Koivisto <tkoivisto456@gmail.com>
2020-01-06 19:32:29 +0200

    Update db-migrate script

--- cf-db-migrate.sh
+++ cf-db-migrate.sh
@@ -12,71 +12,96 @@
 # fail or are missing something, the only correct way is to either create a new one with "Vx_x__xx" or undo one
 # "Ux_x_xx". This is because Flyway stores the last ran migration's version number, and won't run any new migrations
 # below that number even if their files are changed.
-#
-# Usage: AWS_PROFILE=x ./cf-db-migrate.sh ?(dev|prod) ?x
-# where 1st argument is the environment which is either dev or prod
-# and 2nd argument any string to pass the checksum check (incase the previous migration
-# ECS task failed but updated the checksum)
+#####
+
+##### Sceptre specific attributes
+
+# AWS account id with access to push to the ECR, used in the CloudFormation stacks ecr.yaml-template
+# Default value is my own id
+AWS_ACCOUNT_ID="014750007983"
+AWS_REGION="eu-west-1"
+# Project name as in the Sceptre configs
+PROJECT="example-app"
+# Default environment is dev
+ENVIRONMENT="dev"
+
+# Repo name or whatever of the repository as in the ecr.yaml-template
+ECR_APP_NAME="migration"
+# Defined in the ecr.yaml RepositoryName as ${Project}-${Environment}/${AppName}
+ECR_REPOSITORY=${PROJECT}-${ENVIRONMENT}/${ECR_APP_NAME}
+REGISTRY_URL=${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com
+IMAGE_TAG="latest"
+# Eg 014750007983.dkr.ecr.eu-west-1.amazonaws.com/example-app-dev/migration:latest
+IMAGE_WITH_LATEST_TAG=${REGISTRY_URL}/${ECR_REPOSITORY}:${IMAGE_TAG}
+
+# From the ecs-service.yaml-template
+ECS_SERVICE_NAME="migration"
+# The to-be-updated Sceptre stack (and only its "Image" parameter)
+TEMPLATE_NAME="ecs-db-migration"
+# Probably example-app-dev-eu-west-1-ecs-db-migration
+CF_STACK_NAME=${PROJECT}-${ENVIRONMENT}-${AWS_REGION}-${TEMPLATE_NAME}
+
+MIGRATION_CHECKSUM_SSM_PATH="/example-app/dev/migration-checksum"
+SKIP_MIGRATION_CHECKSUM=false
 #####
 
 print_red() {
   printf "\033[1;31m$1\033[0m\n"
 }
 
-if [ -z "$1" ]; then
-  print_red "No environment provided, using dev-environment"
-  ENVIRONMENT="dev"
-else
-  ENVIRONMENT=$1
-fi
+usage_migration() {
+  cat << USAGE >&2
+Usage:
+    $0 ?[-e=ENV] ?[-id=ID] ?[-s]
+    -e=ENV | --env=ENV          Environment, either dev or prod. Default is dev
+    -id=ID | --aws-id=ID        AWS account id where your stacks are launched. Used for ECR pull access. I'm using my own
+                                AWS account id as default value since I'm lazy and probably only one who'll ever use this.
+    -s | --skip-checksum        Skip migration checksum when deploying ECS migration task (incase previous task errored)
+USAGE
+}
 
-if [ -z "$2" ]; then
-  SKIP_CHECKSUM=false
-else
-  SKIP_CHECKSUM=true
-fi
-
-##### Sceptre specific attributes
-# AWS account id with access to push to the ECR, defined in the CloudFormation stacks ecr.yaml-template
-AWS_ACCOUNT_ID="014750007983"
-AWS_REGION="eu-west-1"
-# From the Sceptre config.yaml
-PROJECT="example-app"
-# From the ecr.yaml-template
-ECR_APP_NAME="migration"
-# From the ecs-service.yaml-template
-ECS_SERVICE_NAME="migration"
-# Defined in the ecr.yaml RepositoryName with ${Project}-${Environment}/${ExampleNodejsAppName}
-ECR_REPOSITORY=${PROJECT}-${ENVIRONMENT}/${ECR_APP_NAME}
-REGISTRY_URL=${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com
-IMAGE_TAG="latest"
-# The to-be-updated Sceptre stack (and only its "Image" parameter)
-TEMPLATE_NAME="ecs-db-migration"
-CF_STACK_NAME=${PROJECT}-${ENVIRONMENT}-${AWS_REGION}-${TEMPLATE_NAME} # Probably example-app-dev-eu-west-1-ecs-service
-MIGRATIONCHECKSUM_SSM_PATH="/example-app/dev/migration-checksum"
-#####
+# Parse the arguments
+for i in "$@"; do
+  case $i in
+    -e=*|--env=*)
+      ENVIRONMENT="${i#*=}"
+      shift
+    ;;
+    -id=*|--aws-id=*)
+      AWS_ACCOUNT_ID="${i#*=}"
+      shift
+    ;;
+    -s|--skip-checksum)
+      SKIP_MIGRATION_CHECKSUM=true
+      shift
+    ;;
+    *)
+      print_red "Unknown option '$i'"
+      usage_migration
+      exit 1
+    ;;
+  esac
+done
 
 # First check if the current checksum of migration sql files matches the one in use
 CHECKSUM=$(find -s ./db/migrations -type f | md5)
 CURRENT_CHECKSUM=$(aws ssm get-parameter \
-  --name ${MIGRATIONCHECKSUM_SSM_PATH} \
+  --name ${MIGRATION_CHECKSUM_SSM_PATH} \
   --region ${AWS_REGION} \
   --output text \
   --query Parameter.Value)
 
-if [ "$CHECKSUM" = "$CURRENT_CHECKSUM" ] && [ $SKIP_CHECKSUM = false ]; then
+if [ "$SKIP_MIGRATION_CHECKSUM" = true ]; then
+  print_red "0) Skip migration checksum check - running the migrations"
+elif [ "$CHECKSUM" = "$CURRENT_CHECKSUM" ]; then
   print_red "0) Migrations checksum unchanged - skip migrations"
   exit 0
+else
+  print_red "0) Checksum changed - running the migrations"
 fi
 
-print_red "0) Running the migrations"
-
-# Eg 014750007983.dkr.ecr.eu-west-1.amazonaws.com/example-app-dev/migration:latest
-IMAGE_WITH_LATEST_TAG=${REGISTRY_URL}/${ECR_REPOSITORY}:${IMAGE_TAG}
-
-set +x
+set +x # Disable echoing if enabled, since the output is ECR docker login & password
 eval $(aws ecr get-login --no-include-email --region ${AWS_REGION})
-# set -x
 
 print_red "1) Building the new Docker images with '${IMAGE_TAG}' tag"
 docker build -t ${IMAGE_WITH_LATEST_TAG} -f ./db/Dockerfile ./db
@@ -133,4 +158,4 @@ aws ecs run-task \
   --network-configuration "awsvpcConfiguration={subnets=[${PRIVATE_SUBNET_ID}],securityGroups=[${APP_SECURITY_GROUP_ID}]}" \
   --cluster ${ECS_CLUSTER_ID}
 
-print_red "5) Migrations finished"
+print_red "5) Migration ECS task running, finishes in approximately 1 minute"
